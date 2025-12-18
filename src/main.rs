ed191f49e66877a3ed99fc4203132e9f90b6d11e
joaofelipegalvao <176946005+joaofelipegalvao@users.noreply.github.com>
2025-12-18 17:20:00 -0300

    feat: v2 - accept input file via CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::env;
 use std::fs;
 
 fn main() {
-    let conteudo = fs::read_to_string("main.rs").unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Uso: count-lines <arquivo>");
+        return;
+    }
+
+    let nome_arquivo = &args[1];
+
+    let conteudo = fs::read_to_string(nome_arquivo).unwrap();
     let linhas = conteudo.lines().count();
+
     println!("{} linhas", linhas);
 }
